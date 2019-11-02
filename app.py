c27f3debdff3003123d389beb43077cb3aaeed8e
Rafael Lino <rafael.veloso.lino@hotmail.com>
2019-11-02 17:50:53 -0300

    Add bd, script to create bd and sql

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ from flask_restful import Resource, Api
 from resources.hotel import Hoteis, Hotel #chamando class hotel em pasta resource e arquivo hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app) #faz o gerenciamento
 
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 if __name__ == '__main__': #flask se nome form principal, rode
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
 # http://127.0.0.1:5000/hoteis
--- resources/hotel.py
+++ resources/hotel.py
@@ -5,6 +5,8 @@ reqparse:
     RequestParser:
         adiciona os argumentos vindos do JSON
 """
+"""
+#lista nao será mais usada
 hoteis = [
     {
         'hotel_id': 'alpha',
@@ -35,64 +37,87 @@ hoteis = [
         'cidade': 'Bahia'
     }
 ]
-
+"""
 
 class Hoteis(Resource):
     #Primeiro recurso da API,
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} #query.all SELECT
         #retorna dict, api flask_restful converte em JSON automaticamente
 
 class Hotel(Resource):
 
     #pega os argumentos vindos da requisição Json
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('cidade')
+    argumentos.add_argument('nome', type=str, required=True, help='The field name cannot be blank')
+    argumentos.add_argument('estrelas', type=float, required=True, help='The field estrelas cannot be blank')
     argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
 
     #procura e retorna hotel
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
+    #def find_hotel(hotel_id):
+    #    for hotel in hoteis:
+    #        if hotel['hotel_id'] == hotel_id:
+    #            return hotel
+    #    return None
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found'}, 404 #status errado
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists".format(hotel_id)}, 400 #bad request
         dados = Hotel.argumentos.parse_args()
         #dados chave e valor
         hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        print(type(novo_hotel))
+        try:
+            hotel_objeto.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500 #intel server error
+        #novo_hotel = hotel_objeto.json()
         #**kwargs descompactado vindo o parse
         #novo_hotel = {'hotel_id': hotel_id, **dados}
 
-        hoteis.append(novo_hotel)
+        #hoteis.append(novo_hotel)
         
-        return novo_hotel, 200
+        #return novo_hotel, 200
+        return hotel_objeto.json(), 201
+
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         #novo_hotel = {'hotel_id': hotel_id, **dados}
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201 # created
+        #hotel_objeto = HotelModel(hotel_id, **dados)
+        #novo_hotel = hotel_objeto.json()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            try:
+                hotel_encontrado.save_hotel()
+            except:
+                return {'message': 'An internal error ocurred trying to save hotel.'}, 500 #intel server error
+            return hotel_encontrado.json(), 200
+        #hoteis.append(hotel_objeto)
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500 #intel server error
+        return hotel.json(), 201 # created
 
     def delete(self, hotel_id):
         #para cada hotel na lista, retorna hoteis que não forem iguais ao hotel passado por parâmetro
         #basicamente cria-se uma lista nova sem o hotel passado por parâmetro.
-        global hoteis #Necessário para infromar ao python que hoteis não é a mesma usada na lista
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted.'}
+        #global #hoteis Necessário para infromar ao python que hoteis não é a mesma usada na lista
+        #hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'An internal error ocurred trying to delete hotel.'}, 500 #intel server error
+            return {'message': 'Hotel deleted'}, 200
+        return {'message': 'Hotel not found.'}, 404
